8730e6d7298d2746838e750f4a4ad606ed08649c
Dan Walsh <dwalsh@redhat.com>
2014-03-27 16:54:48 -0400

    Systemd socket activation fixes
    Systemd sets the LISTEN_PID and LISTEN_FDS environment variables to be passed
    to a service when the administrator sets up the service to be socket activated.
    
    The application is supposed to check its current PID versus the LISTEN_PID, if
    they match, the process is supposed to use the "leaked" fds listed in LISTEN_FDS.
    
    When using docker, we want to pass this information on to the first process in the
    container.  Since this process will be running as PID 1, we need to reset the LISTEN_PID
    to pid 1.
    
    Depending on the docker execdriver and whether we are in foreground mode
    we need to pass/leak these FDS into the container.

--- runconfig/parse.go
+++ runconfig/parse.go
@@ -10,6 +10,7 @@ import (
 	"github.com/dotcloud/docker/runtime/execdriver"
 	"github.com/dotcloud/docker/utils"
 	"io/ioutil"
+	"os"
 	"path"
 	"strings"
 )
@@ -195,6 +196,14 @@ func parseRun(cmd *flag.FlagSet, args []string, sysInfo *sysinfo.SysInfo) (*Conf
 		if _, exists := ports[p]; !exists {
 			ports[p] = struct{}{}
 		}
+	}
+
+	/* Configure for socket activation */
+	LISTEN_PID := os.Getenv("LISTEN_PID")
+	LISTEN_FDS := os.Getenv("LISTEN_FDS")
+	if LISTEN_PID == string(os.Getpid()) && LISTEN_FDS != "" {
+		flEnv.Set(fmt.Sprintf("LISTEN_FDS=%s", LISTEN_FDS))
+		flEnv.Set("LISTEN_PID=1")
 	}
 
 	config := &Config{
