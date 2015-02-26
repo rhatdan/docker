4e9f7f5a36e4688f5bb6dc58563f634819c903ee
Dan Walsh <dwalsh@redhat.com>
2015-02-26 09:24:37 -0500

    Add --registry-prepend and --registry-replace with search/pull commands qualifier to docker daemon
        These options setup a search list of registries from which to pull images.
    
        Registries will be searched in reverse order.
    
        --registry-prepend will allow you to add additional registries to docker.io to
        pull images from.
    
        For example
    
        --registry-prepend="foo.com/registry,bar.com/registry"
    
        Would cause a
        docker pull myapp
    
        to search
        bar.com/registry/myapp
        foo.com/registry/myapp
        docker.io.../myapp
    
        This would allow companies and vendors to specify registries which contain
        content that the companies will not allowed to be stored at docker.io
    
        --registry-replace="bar.com/registry"
        Replaces the docker.io registry as the default registry to search.
    
        This would allow a customer to control which sites that images can be pulled from.
    
    Pull and Search commands iterate over a list of registries given with
    options --registry-prepend or --registry-replace. In case of pull command
    the iteration stops when a matching image is found. In case of search
    command the iteration never stops - all the results are accumulated.
    
    Docker-DCO-1.1-Signed-off-by: Michal Minar <miminar@redhat.com>
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- integration-cli/docker_cli_run_test.go
+++ integration-cli/docker_cli_run_test.go
@@ -891,14 +891,17 @@ func TestRunEnvironmentErase(t *testing.T) {
 	goodEnv := []string{
 		"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 		"HOME=/root",
+		"container_uuid=ID",
 	}
 	sort.Strings(goodEnv)
 	if len(goodEnv) != len(actualEnv) {
-		t.Fatalf("Wrong environment: should be %d variables, not: %q\n", len(goodEnv), strings.Join(actualEnv, ", "))
+		t.Fatalf("Wrong environment: should be %d variables, not %d: %q\n", len(goodEnv), len(actualEnv), strings.Join(actualEnv, ", "))
 	}
 	for i := range goodEnv {
 		if actualEnv[i] != goodEnv[i] {
-			t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			if strings.Split(actualEnv[i], "=")[0] != "container_uuid" {
+				t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			}
 		}
 	}
 
@@ -938,7 +941,9 @@ func TestRunEnvironmentOverride(t *testing.T) {
 	}
 	for i := range goodEnv {
 		if actualEnv[i] != goodEnv[i] {
-			t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			if strings.Split(actualEnv[i], "=")[0] != "container_uuid" {
+				t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			}
 		}
 	}
 
--- runconfig/parse.go
+++ runconfig/parse.go
@@ -63,7 +63,7 @@ func Parse(cmd *flag.FlagSet, args []string) (*Config, *HostConfig, *flag.FlagSe
 		flWorkingDir      = cmd.String([]string{"w", "-workdir"}, "", "Working directory inside the container")
 		flCpuShares       = cmd.Int64([]string{"c", "-cpu-shares"}, 0, "CPU shares (relative weight)")
 		flCpuset          = cmd.String([]string{"-cpuset"}, "", "CPUs in which to allow execution (0-3, 0,1)")
-		flNetMode         = cmd.String([]string{"-net"}, "bridge", "Set the Network mode for the container\n'bridge': creates a new network stack for the container on the Docker bridge\n'none': no networking for this container\n'container:<name|id>': reuses another container network stack\n'host': use the host network stack inside the container.  Note: the host mode gives the container full access to local system services such as D-bus and is therefore considered insecure.\n'ns:<path>': use the specified network namespace")
+		flNetMode         = cmd.String([]string{"-net"}, "bridge", "Set the Network mode for the container")
 		flMacAddress      = cmd.String([]string{"-mac-address"}, "", "Container MAC address (e.g. 92:d0:c6:0a:29:33)")
 		flIpcMode         = cmd.String([]string{"-ipc"}, "", "IPC namespace to use")
 		flRestartPolicy   = cmd.String([]string{"-restart"}, "", "Restart policy to apply when a container exits")
