3ade1e5717e0cf88efd9a14d6ff90dbd9815c16b
Michal Minar <miminar@redhat.com>
2015-03-09 14:22:03 +0100

    Confirm a push to public Docker registry
    Ask the user to confirm a push of image to public Docker registry. The
    confirmation can be suppressed with the use of `-f, --force` flag.
    
    Added --confirm-def-push boolean flag to Docker daemon. It causes
    client to ask for confirmation before a push to public registry. It
    defaults to true. If set to false, push won't be restricted in any
    way unless the registry in question is blocked.
    
    No confirmation is needed for push to unofficial registry.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- daemon/config.go
+++ daemon/config.go
@@ -37,6 +37,7 @@ type CommonConfig struct {
 	NetworkKVStore       string
 	BlockedRegistries    []string
 	AdditionalRegistries []string
+	ConfirmDefPush       bool
 }
 
 // InstallCommonFlags adds command-line options to the top-level flag parser for
@@ -61,6 +62,5 @@ func (config *Config) InstallCommonFlags(cmd *flag.FlagSet, usageFn func(string)
 	cmd.Var(opts.NewListOptsRef(&config.Labels, opts.ValidateLabel), []string{"-label"}, usageFn("Set key=value labels to the daemon"))
 	cmd.StringVar(&config.LogConfig.Type, []string{"-log-driver"}, "json-file", usageFn("Default driver for container logs"))
 	cmd.Var(opts.NewMapOpts(config.LogConfig.Config, nil), []string{"-log-opt"}, usageFn("Set log driver options"))
-	cmd.Var(opts.NewListOptsRef(&config.BlockedRegistries, nil), []string{"-block-registry"}, usageFn("Don't contact given registry"))
-	cmd.Var(opts.NewListOptsRef(&config.AdditionalRegistries, nil), []string{"-add-registry"}, usageFn("Registry to query before a public one"))
+	cmd.BoolVar(&config.ConfirmDefPush, []string{"-confirm-def-push"}, true, usageFn("Confirm a push to default registry"))
 }
