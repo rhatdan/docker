a33dd65937b61203487a73e5af77e595574b6e3e
Michal Minar <miminar@redhat.com>
2015-03-09 14:22:03 +0100

    Confirm a push to public Docker registry
    Ask the user to confirm a push of image to public Docker registry. The
    confirmation can be suppressed with the use of `-f, --force` flag.
    
    No confirmation is needed for push to unofficial registry.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- api/server/server.go
+++ api/server/server.go
@@ -211,6 +211,7 @@ func httpError(w http.ResponseWriter, err error) {
 		"impossible":            http.StatusNotAcceptable,
 		"wrong login/password":  http.StatusUnauthorized,
 		"hasn't been activated": http.StatusForbidden,
+		"needs to be forced":    http.StatusForbidden,
 	} {
 		if strings.Contains(errStr, keyword) {
 			statusCode = status
@@ -859,6 +860,7 @@ func (s *Server) postImagesPush(eng *engine.Engine, version version.Version, w h
 		Tag:         r.Form.Get("tag"),
 		OutStream:   output,
 		Json:        useJSON,
+		Force:       boolValue(r, "force"),
 	}
 	if useJSON {
 		w.Header().Set("Content-Type", "application/json")
