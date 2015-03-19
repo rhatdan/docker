d015e1a0a256d99486b2b996b12bacb5f56aad72
Dan Walsh <dwalsh@redhat.com>
2015-03-19 15:16:49 -0400

    Have docker register its machine with systemd
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- daemon/container.go
+++ daemon/container.go
@@ -39,6 +39,7 @@ import (
 	"github.com/docker/docker/pkg/resolvconf"
 	"github.com/docker/docker/pkg/stringid"
 	"github.com/docker/docker/pkg/symlink"
+	"github.com/docker/docker/pkg/systemd"
 	"github.com/docker/docker/pkg/ulimit"
 	"github.com/docker/docker/runconfig"
 	"github.com/docker/docker/utils"
@@ -452,7 +453,13 @@ func (container *Container) Start() (err error) {
 		return err
 	}
 
-	return container.waitForStart()
+	if err := container.waitForStart(); err != nil {
+		return err
+	}
+
+	container.registerMachine()
+
+	return nil
 }
 
 func (container *Container) Run() error {
@@ -1534,4 +1541,17 @@ func (c *Container) LogDriverType() string {
 		return c.daemon.defaultLogConfig.Type
 	}
 	return c.hostConfig.LogConfig.Type
+}
+
+/*
+Register Machine with systemd.  There is a potential race condition here
+where the container could have exited before the call gets made.  This
+call requires the container.Pid.  Therefore we just log the situation
+rather then fail the container
+*/
+func (container *Container) registerMachine() {
+	err := systemd.RegisterMachine(container.Name[1:], container.ID, container.Pid, "/")
+	if err != nil {
+		logrus.Errorf("Unable to RegisterMachine %s for %s: %s", container.Name[1:], container.ID, err)
+	}
 }
