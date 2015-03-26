b7aaa15b614193f245ee3f57876563a0b536c769
Dan Walsh <dwalsh@redhat.com>
2015-03-26 15:46:36 -0400

    Set container_uuid environment
    If you are running with systemd as init 1 and specify the container_uuid environment
    variable, systemd will create /etc/machine-id with the correct data.
    
    Then on the host you can setup journald to watch the container.
    
    systemd only allows 32 chars in the UUID stored in /etc/machine-id
    
    Then we can later apply a different patch to setup journald to watch
    containers from the host and log all syslog/stdout/stderr data together
    int the hosts journal.
    
    The following link explains what systemd expects to be setup.
    
    http://www.freedesktop.org/wiki/Software/systemd/ContainerInterface/
    
    This should replace
    
    https://github.com/docker/docker/issues/3506
    
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- integration-cli/docker_cli_run_test.go
+++ integration-cli/docker_cli_run_test.go
@@ -936,14 +936,17 @@ func TestRunEnvironment(t *testing.T) {
 		"cky",
 		"",
 		"HOME=/root",
+		"container_uuid=ID",
 	}
 	sort.Strings(goodEnv)
 	if len(goodEnv) != len(actualEnv) {
-		t.Fatalf("Wrong environment: should be %d variables, not: %q\n", len(goodEnv), strings.Join(actualEnv, ", "))
+		t.Fatalf("Wrong environment: should be %d variables, not %d: %q\n", len(goodEnv), len(actualEnv), strings.Join(actualEnv, ", "))
 	}
 	for i := range goodEnv {
 		if actualEnv[i] != goodEnv[i] {
-			t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			if strings.Split(actualEnv[i], "=")[0] != "container_uuid" {
+				t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			}
 		}
 	}
 
@@ -976,6 +979,7 @@ func TestRunEnvironmentErase(t *testing.T) {
 	goodEnv := []string{
 		"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 		"HOME=/root",
+		"container_uuid=ID",
 	}
 	sort.Strings(goodEnv)
 	if len(goodEnv) != len(actualEnv) {
@@ -983,7 +987,9 @@ func TestRunEnvironmentErase(t *testing.T) {
 	}
 	for i := range goodEnv {
 		if actualEnv[i] != goodEnv[i] {
-			t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			if strings.Split(actualEnv[i], "=")[0] != "container_uuid" {
+				t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			}
 		}
 	}
 
@@ -1016,6 +1022,7 @@ func TestRunEnvironmentOverride(t *testing.T) {
 		"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 		"HOME=/root2",
 		"HOSTNAME=bar",
+		"container_uuid=ID",
 	}
 	sort.Strings(goodEnv)
 	if len(goodEnv) != len(actualEnv) {
@@ -1023,7 +1030,9 @@ func TestRunEnvironmentOverride(t *testing.T) {
 	}
 	for i := range goodEnv {
 		if actualEnv[i] != goodEnv[i] {
-			t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			if strings.Split(actualEnv[i], "=")[0] != "container_uuid" {
+				t.Fatalf("Wrong environment variable: should be %s, not %s", goodEnv[i], actualEnv[i])
+			}
 		}
 	}
 
