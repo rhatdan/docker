a0bbe34fc6ec252bfcd2afd1307d25b66f9e3929
Michal Minar <miminar@redhat.com>
2015-04-07 16:35:25 +0200

    Preserve tag passed on command-line
    Do not fully-qualify tags given on command line:
    
        $ docker tag repo/name dest/name
    
    Prior to this patch `dest/name` would be prefixed with default registry.
    Now only pulled images will be fully-qualified.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- graph/tag.go
+++ graph/tag.go
@@ -12,7 +12,7 @@ func (s *TagStore) CmdTag(job *engine.Job) engine.Status {
 	if len(job.Args) == 3 {
 		tag = job.Args[2]
 	}
-	if err := s.Set(job.Args[1], tag, job.Args[0], job.GetenvBool("force"), false); err != nil {
+	if err := s.Set(job.Args[1], tag, job.Args[0], job.GetenvBool("force"), true); err != nil {
 		return job.Error(err)
 	}
 	return engine.StatusOK
