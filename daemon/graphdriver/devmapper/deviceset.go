3c40e975cd9fa4bb3fe19d177c205646039d5619
Dan Walsh <dwalsh@redhat.com>
2015-04-09 09:00:07 -0400

    Change mount point propogation to default to slave
    As an admin I would expect that if I volume mount a partition into a container, and then later add a mount point to the volume, it will show up in the container.
    
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- daemon/graphdriver/devmapper/deviceset.go
+++ daemon/graphdriver/devmapper/deviceset.go
@@ -1087,12 +1087,11 @@ func (devices *DeviceSet) initDevmapper(doInit bool) error {
 
 	if devices.thinPoolDevice == "" {
 		if devices.metadataLoopFile != "" || devices.dataLoopFile != "" {
-			logrus.Errorf("WARNING: No --storage-opt dm.thinpooldev specified, using loopback; this configuration is strongly discouraged for production use")
-		}  else {
-			logrus.Warnf("--storage-opt dm.thinpooldev is preferred over --storage-opt dm.datadev or dm.metadatadev")
+			log.Errorf("WARNING: No --storage-opt dm.thinpooldev specified, using loopback; this configuration is strongly discouraged for production use")
+		} else {
+			log.Warnf("--storage-opt dm.thinpooldev is preferred over --storage-opt dm.datadev or dm.metadatadev")
 		}
-	}		
-
+	}
 
 	// If we didn't just create the data or metadata image, we need to
 	// load the transaction id and migrate old metadata
