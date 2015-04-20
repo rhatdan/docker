99ac4d61c68a3c4dea67d6f5646eea49fe1accc6
Dan Walsh <dwalsh@redhat.com>
2015-04-20 13:18:02 -0400

    New implementation of /run support
        This mounts a /run tmpfs into the container, with the initial contents
        copies from the /run in the base image, unless MountRun is set to false
        in the HostConfig.
    
        Additionally MountRun is always set to false during a docker build, which i
        means any setup of /run in a Dockerfile is saved in the image to be copied
        into the final /run tmpfs when a container is started.
    
        Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)
    
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- runconfig/hostconfig.go
+++ runconfig/hostconfig.go
@@ -188,6 +188,7 @@ type HostConfig struct {
 	Ulimits         []*ulimit.Ulimit
 	LogConfig       LogConfig
 	CgroupParent    string // Parent cgroup.
+	MountRun        bool
 }
 
 func MergeConfigs(config *Config, hostConfig *HostConfig) *ContainerConfigWrapper {
