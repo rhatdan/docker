70aa844a49f568e972b9005a96103aca0c833ba8
Michal Minar <miminar@redhat.com>
2015-05-11 07:45:14 +0200

    Allow for remote repository inspection
    **Inspect remote images**
    
    Extended server API to allow for inspection of remote images. If a short
    name is given, list of additional registries will be queried until a
    repository is found. This allows for remote inspection without
    downloading image layers.
    
    The URL is following:
    
        /<apiVersion>/images/<repoName>/json?remote=1
    
    The output is the same json as for local images. However, it excludes
    few attributes specific only to local images:
    
    - GraphDriver
    - VirtualSize
    
    And it adds some additional attributes:
    
    - Digest
    - Tag
    - Registry
    
    **List tags of remote repository**
    
    Extended server API to allow for listing of remote repositories.
    
    The URL is following:
    
        /<apiVersion>/images/<repoName>/tags?remote=1
    
    If a short name is given, list of additional registries will be queried
    until a repository is found.
    
    Returned is a json in following format:
    
        { "Name"    : "<repoName>"
        , "TagList" : [ {"<tag1>" : "<ID1>", "<tag2>" : <ID2>" } ]
        }
    
    Quirks and TODOs:
     - By default V1 registry enpoints are queried because they provide tags
       with associated image IDs. If not available, V2 endpoint will be
       tried. In such a case the output will lack image IDs though.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- api/server/server.go
+++ api/server/server.go
@@ -1267,6 +1267,51 @@ func (s *Server) getImagesByName(version version.Version, w http.ResponseWriter,
 	return writeJSON(w, http.StatusOK, imageInspect)
 }
 
+func (s *Server) getImagesTags(version version.Version, w http.ResponseWriter, r *http.Request, vars map[string]string) error {
+	name := vars["name"]
+	authEncoded := r.Header.Get("X-Registry-Auth")
+	authConfig := &cliconfig.AuthConfig{}
+	if authEncoded != "" {
+		authJSON := base64.NewDecoder(base64.URLEncoding, strings.NewReader(authEncoded))
+		if err := json.NewDecoder(authJSON).Decode(authConfig); err != nil {
+			// for a pull it is not an error if no auth was given
+			// to increase compatibility with the existing api it is defaulting to be empty
+			authConfig = &cliconfig.AuthConfig{}
+		}
+	}
+
+	var (
+		tagList *types.RepositoryTagList
+		err     error
+	)
+	if !boolValue(r, "remote") {
+		tagList, err = s.daemon.Repositories().Tags(name)
+		if err != nil {
+			logrus.Warnf("failed to get local tags for %q", name)
+		}
+	}
+	if tagList == nil || err != nil {
+		metaHeaders := map[string][]string{}
+		for k, v := range r.Header {
+			if strings.HasPrefix(k, "X-Meta-") {
+				metaHeaders[k] = v
+			}
+		}
+		tagsConfig := &graph.RemoteTagsConfig{
+			MetaHeaders: metaHeaders,
+			AuthConfig:  authConfig,
+		}
+		tagList, err = s.daemon.Repositories().RemoteTags(name, tagsConfig)
+		if err != nil {
+			logrus.Warnf("failed to get remote tags for %q: %v", name, err)
+		}
+	}
+	if err != nil {
+		return err
+	}
+	return writeJSON(w, http.StatusOK, tagList)
+}
+
 func (s *Server) postBuild(version version.Version, w http.ResponseWriter, r *http.Request, vars map[string]string) error {
 	var (
 		authConfigs        = map[string]cliconfig.AuthConfig{}
@@ -1653,6 +1698,7 @@ func createRouter(s *Server) *mux.Router {
 			"/images/{name:.*}/get":           s.getImagesGet,
 			"/images/{name:.*}/history":       s.getImagesHistory,
 			"/images/{name:.*}/json":          s.getImagesByName,
+			"/images/{name:.*}/tags":          s.getImagesTags,
 			"/containers/ps":                  s.getContainersJSON,
 			"/containers/json":                s.getContainersJSON,
 			"/containers/{name:.*}/export":    s.getContainersExport,
