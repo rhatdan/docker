be690afe52ac16aabde06b168ba14eddb149fa86
Dan Walsh <dwalsh@redhat.com>
2015-05-27 20:26:25 -0400

    Have docker register its machine with systemd
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- daemon/container.go
+++ daemon/container.go
@@ -351,6 +351,7 @@ func (container *Container) cleanup() {
 	}
 
 	container.UnmountVolumes(false)
+	container.terminateMachine()
 }
 
 func (container *Container) KillSig(sig int) error {
@@ -747,6 +748,7 @@ func (container *Container) waitForStart() error {
 		return err
 	}
 
+	container.registerMachine()
 	return nil
 }
 
