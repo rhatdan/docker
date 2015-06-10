8b1d3d246be03ddaa45a62be1f555040665fcf7a
William Temple <wtemple@redhat.com>
2015-06-10 15:18:50 -0400

    Expose configured registry list via Docker info API.
    Signed-off-by: William Temple <wtemple@redhat.com>
    
    Docker-DCO-1.1-Signed-off-by: William Temple <wtemple@redhat.com> (github: rhatdan)

--- api/types/types.go
+++ api/types/types.go
@@ -159,6 +159,7 @@ type Info struct {
 	OperatingSystem    string
 	IndexServerAddress string
 	RegistryConfig     interface{}
+	RegistryList       []string
 	InitSha1           string
 	InitPath           string
 	NCPU               int
