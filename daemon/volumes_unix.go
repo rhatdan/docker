4897f83205ae35d6c24e0483e0f66c3264ad0c88
Dan Walsh <dwalsh@redhat.com>
2015-07-28 14:42:04 -0400

    Add RHEL super secrets patch
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- daemon/volumes_unix.go
+++ daemon/volumes_unix.go
@@ -40,6 +40,18 @@ func copyOwnership(source, destination string) error {
 // /etc/resolv.conf, and if it is not, appends it to the array of mounts.
 func (container *Container) setupMounts() ([]execdriver.Mount, error) {
 	var mounts []execdriver.Mount
+
+	secretsPath, err := container.secretsPath()
+	if err != nil {
+		return nil, err
+	}
+
+	mounts = append(mounts, execdriver.Mount{
+		Source:      secretsPath,
+		Destination: "/run/secrets",
+		Writable:    true,
+	})
+
 	for _, m := range container.MountPoints {
 		path, err := m.Setup()
 		if err != nil {
