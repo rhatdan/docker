11e57d913d3bea92e226584d9f98498a88adba7d
Michal Minar <miminar@redhat.com>
2015-08-19 14:46:00 +0200

    Fixed push of unqualified image
    This fixes a case where there is fully qualified and unqualified
    repository with the same name in Docker's local storage e.g:
    
        user/repo
        default.registry.io/user/repo
    
    Fully qualified repository was always preferred over the unqualified.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>
    
    Michal Minar <miminar@redhat.com>

--- integration-cli/docker_cli_push_test.go
+++ integration-cli/docker_cli_push_test.go
@@ -672,3 +672,29 @@ func (s *DockerRegistrySuite) TestPushToAdditionalRegistry(c *check.C) {
 	}
 	d.getAndTestImageEntry(c, 1, s.reg.url+"/library/busybox", busyboxID)
 }
+
+func (s *DockerRegistrySuite) TestPushCustomTagToAdditionalRegistry(c *check.C) {
+	d := NewDaemon(c)
+	if err := d.StartWithBusybox("--add-registry=" + s.reg.url); err != nil {
+		c.Fatalf("we should have been able to start the daemon with passing add-registry=%s: %v", s.reg.url, err)
+	}
+	defer d.Stop()
+
+	busyboxID := d.getAndTestImageEntry(c, 1, "busybox", "").id
+
+	if out, err := d.Cmd("tag", "busybox", "user/busybox:1.2.3"); err != nil {
+		c.Fatalf("failed to tag image %s: error %v, output %q", "busybox", err, out)
+	}
+	if out, err := d.Cmd("tag", "busybox", s.reg.url+"/user/busybox:latest"); err != nil {
+		c.Fatalf("failed to tag image %s: error %v, output %q", "busybox", err, out)
+	}
+	if out, err := d.Cmd("push", "user/busybox:1.2.3"); err != nil {
+		c.Fatalf("failed to push image user/busybox: error %v, output %q", err, out)
+	}
+	d.getAndTestImageEntry(c, 3, "user/busybox", busyboxID)
+	toRemove := []string{"user/busybox:1.2.3"}
+	if out, err := d.Cmd("rmi", toRemove...); err != nil {
+		c.Fatalf("failed to remove images %v: %v, output: %s", toRemove, err, out)
+	}
+	d.getAndTestImageEntry(c, 2, s.reg.url+"/user/busybox", busyboxID)
+}
