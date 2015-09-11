4e8c872f443cd74ed78bfbc43e0908c681d41af0
Dan Walsh <dwalsh@redhat.com>
2015-09-11 06:14:41 -0400

    Allow for remote repository inspection
    **Inspect remote images**
    
    Extended server API to allow for inspection of remote images. If a short
    name is given, list of additional registries will be queried until a
    repository is found. This allows for remote inspection without
    downloading image layers.
    
    The URL is following:
    
        /<apiVersion>/images/<repoName>/json?remote=1
    
    The output is the same json as for local images. However, it excludes
    few attributes specific only to local images:
    
    - GraphDriver
    - VirtualSize
    
    And it adds some additional attributes:
    
    - Digest
    - Tag
    - Registry
    
    **List tags of remote repository**
    
    Extended server API to allow for listing of remote repositories.
    
    The URL is following:
    
        /<apiVersion>/images/<repoName>/tags?remote=1
    
    If a short name is given, list of additional registries will be queried
    until a repository is found.
    
    Returned is a json in following format:
    
        { "Name"    : "<repoName>"
        , "TagList" : [ {"<tag1>" : "<ID1>", "<tag2>" : <ID2>" } ]
        }
    
    Quirks and TODOs:
     - By default V1 registry enpoints are queried because they provide tags
       with associated image IDs. If not available, V2 endpoint will be
       tried. In such a case the output will lack image IDs though.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>
    
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)
    
    Dan Walsh <dwalsh@redhat.com>

--- api/types/types.go
+++ api/types/types.go
@@ -92,9 +92,9 @@ type GraphDriverData struct {
 	Data map[string]string
 }
 
-// ImageInspect contains response of Remote API:
+// ImageInspectBase contains response of Remote API:
 // GET "/images/{name:.*}/json"
-type ImageInspect struct {
+type ImageInspectBase struct {
 	ID              string `json:"Id"`
 	Tags            []string
 	Parent          string
@@ -108,8 +108,21 @@ type ImageInspect struct {
 	Architecture    string
 	Os              string
 	Size            int64
-	VirtualSize     int64
-	GraphDriver     GraphDriverData
+}
+
+// GET "/images/{name:.*}/json"
+type ImageInspect struct {
+	ImageInspectBase
+	VirtualSize int64
+	GraphDriver GraphDriverData
+}
+
+// GET "/images/{name:.*}/json?remote=1"
+type RemoteImageInspect struct {
+	ImageInspectBase
+	Registry string
+	Digest   string
+	Tag      string
 }
 
 // Port stores open ports info of container
@@ -340,4 +353,16 @@ type VolumeCreateRequest struct {
 	Name       string            // Name is the requested name of the volume
 	Driver     string            // Driver is the name of the driver that should be used to create the volume
 	DriverOpts map[string]string // DriverOpts holds the driver specific options to use for when creating the volume.
+}
+
+type RepositoryTag struct {
+	Tag     string
+	ImageID string
+}
+
+// GET "/images/{name:.*}/tags"
+type RepositoryTagList struct {
+	// Fully qualified repository name
+	Name    string
+	TagList []*RepositoryTag
 }
