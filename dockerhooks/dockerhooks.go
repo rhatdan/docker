96477dcc568e70085ef32028d99e49af3bb0a65c
Dan Walsh <dwalsh@redhat.com>
2015-10-05 14:56:12 -0400

    Add dockerhooks program to run hooks under runc
    With the addition of runc/hooks support we want to add a feature
    to allow third parties to run helper programs before a docker container
    gets started and just after the container finishes.
    
    For example we want to add a RegisterMachine hook.
    
    For systems that support systemd/RegisterMachine, this hook would register
    a machine to the machinectl.  machinectl could then list docker containers
    along with other virtulization environments like kvm, and systemd-nspawn
    containers. Overtime we would want to implement other machinectl features
    to get docker containers better integrated into the system and machinectl.
    
    Another example of a dockerhook might be for people wanting to do better logging
    of starting and stopping of containers.  For example have a log agent that
    records when a container starts and stops and then sends a message to a
    monitoring station.
    
    Dockerhooks reads an environment variable DOCKER_HOOKS_PATH for a directory
    to search for hooks, if the directory does not exist dockerhooks just exits.
    
    docker daemon uses the search mechanism for dockerinit to look for dockerhooks
    it also sets the DOCKER_HOOKS_PATH based off the directory containers dockerhooks.
    
    and will execute them via runc/libcontainer using PreStart and PostStop.
    
    Signed-off-by: Sally O'Malley <somalley@redhat.com>
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

---
+++ dockerhooks/dockerhooks.go
@@ -0,0 +1,106 @@
+// +build linux
+
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"log/syslog"
+	"os"
+	"os/exec"
+	"path"
+)
+
+func getHooks() (string, []os.FileInfo, error) {
+	hookPath := os.Getenv("DOCKER_HOOKS_PATH")
+	if hookPath == "" {
+		hookPath = "hooks.d"
+	}
+	// find any hooks executables
+	if _, err := os.Stat(hookPath); os.IsNotExist(err) {
+		return "", nil, nil
+	}
+	hooks, err := ioutil.ReadDir(hookPath)
+	return hookPath, hooks, err
+}
+
+func prestart(hooks []os.FileInfo, hookPath string, stdinbytes []byte) error {
+	for _, item := range hooks {
+		if item.Mode().IsRegular() {
+			if err := run(path.Join(hookPath, item.Name()), stdinbytes); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func poststop(hooks []os.FileInfo, hookPath string, stdinbytes []byte) error {
+	for i := len(hooks) - 1; i >= 0; i-- {
+		fn := hooks[i].Name()
+		if hooks[i].Mode().IsRegular() {
+			if err := run(path.Join(hookPath, fn), stdinbytes); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func run(hookfile string, incoming []byte) error {
+	cmd := exec.Command(hookfile)
+	cmd.Args = os.Args
+	cmd.Env = os.Environ()
+	log.Print("Run docker hook: ", hookfile, cmd.Args, cmd.Env)
+	stdinPipe, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	stdinPipe.Write(incoming)
+	return nil
+}
+
+func main() {
+	var (
+		err       error
+		hooks     []os.FileInfo
+		incoming  []byte
+		hookPath  string
+		logwriter *syslog.Writer
+	)
+
+	logwriter, err = syslog.New(syslog.LOG_NOTICE, "docker-hooks")
+	if err == nil {
+		log.SetOutput(logwriter)
+	}
+	log.Print("Executing dockerhook ", os.Args[0])
+
+	if hookPath, hooks, err = getHooks(); err != nil {
+		log.Fatalf("dockerhooks error getHooks Failed: %v", err)
+	}
+	if len(hooks) == 0 {
+		return
+	}
+
+	incoming, err = ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("dockerhooks error ioutil.ReadAll Failed: %v", err)
+	}
+
+	switch os.Args[0] {
+	case "prestart":
+		if err := prestart(hooks, hookPath, incoming); err != nil {
+			log.Fatalf("dockerhooks prestart %s Failed: %v", hooks, err)
+		}
+	case "poststop":
+		if err := poststop(hooks, hookPath, incoming); err != nil {
+			log.Fatalf("dockerhooks prestart %s Failed: %v", hooks, err)
+		}
+	default:
+		log.Fatalf("ERROR: Invalid argument %v", os.Args[0])
+	}
+}
