41b9c126d7684d8c0f71229338c997eda9555969
Sally O'Malley <somalley@redhat.com>
2015-10-20 12:22:00 -0400

    fix go-lint, go-vet errs in audit log patch
    Signed-off-by: Sally O'Malley <somalley@redhat.com>

--- pkg/audit/audit_linux.go
+++ pkg/audit/audit_linux.go
@@ -1,4 +1,5 @@
 // +build linux
+
 /*
   The audit package is a go bindings to libaudit that only allows for
   logging audit events.
@@ -21,13 +22,15 @@ import (
 	"unsafe"
 )
 
+// AuditVert* constants used when logging an API event to the audit log
 const (
-	AUDIT_VIRT_CONTROL    = 2500
-	AUDIT_VIRT_RESOURCE   = 2501
-	AUDIT_VIRT_MACHINE_ID = 2502
+	AuditVertControl   = 2500
+	AuditVertResource  = 2501
+	AuditVertMachineID = 2502
 )
 
-func AuditValueNeedsEncoding(str string) bool {
+// ValueNeedsEncoding returns true if audit value needs encoding
+func ValueNeedsEncoding(str string) bool {
 	cstr := C.CString(str)
 	defer C.free(unsafe.Pointer(cstr))
 	len := C.strlen(cstr)
@@ -39,7 +42,8 @@ func AuditValueNeedsEncoding(str string) bool {
 	return false
 }
 
-func AuditEncodeNVString(name string, value string) string {
+// EncodeNVString returns string of encoded audit value
+func EncodeNVString(name string, value string) string {
 	cname := C.CString(name)
 	cval := C.CString(value)
 
@@ -52,7 +56,8 @@ func AuditEncodeNVString(name string, value string) string {
 	return C.GoString(cres)
 }
 
-func AuditLogUserEvent(event_type int, message string, result bool) error {
+// LogUserEvent logs info msg from user event or error
+func LogUserEvent(eventType int, message string, result bool) error {
 	var r int
 	fd := C.audit_open()
 	defer C.close(fd)
@@ -64,13 +69,14 @@ func AuditLogUserEvent(event_type int, message string, result bool) error {
 	if fd > 0 {
 		cmsg := C.CString(message)
 		defer C.free(unsafe.Pointer(cmsg))
-		_, err := C.audit_log_user_message(fd, C.int(event_type), cmsg, nil, nil, nil, C.int(r))
+		_, err := C.audit_log_user_message(fd, C.int(eventType), cmsg, nil, nil, nil, C.int(r))
 		return err
 	}
 	return nil
 }
 
-func AuditFormatVars(vars map[string]string) string {
+// FormatVars formats map to string
+func FormatVars(vars map[string]string) string {
 	var result string
 	for key, value := range vars {
 		result += fmt.Sprintf("%s=%s ", key, value)
