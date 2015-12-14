1f32c3ce38494346dcf0801c2856ab28f98e42ef
Dan Walsh <dwalsh@redhat.com>
2015-12-14 17:16:20 -0500

    System logging for docker daemon API calls
    As part of handling the middleware wrappers around API calls requested
    by clients, log information about the request and the user's identity
    (UID, login name if we can find it, and PID) to syslog.
    
    Signed-off-by: Alec Benson <albenson@redhat.com> (github: alecbenson)
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com> (github: nalind)
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>
    
    System logging for docker daemon API calls
    
    As part of handling the middleware wrappers around API calls requested
    by clients, log information about the request and the user's identity
    (UID, login name if we can find it, and PID) to syslog.
    
    Signed-off-by: Alec Benson <albenson@redhat.com> (github: alecbenson)
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com> (github: nalind)
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

--- api/server/server.go
+++ api/server/server.go
@@ -42,6 +42,7 @@ type Config struct {
 // Server contains instance details for the server
 type Server struct {
 	cfg          *Config
+	daemon       *daemon.Daemon
 	servers      []*HTTPServer
 	routers      []router.Router
 	authZPlugins []authorization.Plugin
@@ -77,6 +78,11 @@ func (s *Server) Close() {
 			logrus.Error(err)
 		}
 	}
+}
+
+// SetDaemon initializes the daemon field
+func (s *Server) SetDaemon(daemon *daemon.Daemon) {
+	s.daemon = daemon
 }
 
 // ServeAPI loops through all initialized servers and spawns goroutine
