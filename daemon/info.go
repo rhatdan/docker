595567d3a402a7378194316a3a076ec3e0201386
Dan Walsh <dwalsh@redhat.com>
2016-01-05 08:11:05 -0500

    Return rpm version of packages in docker version
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)
    
    Dan Walsh <dwalsh@redhat.com>
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

--- daemon/info.go
+++ daemon/info.go
@@ -12,6 +12,7 @@ import (
 	"github.com/docker/docker/pkg/parsers/kernel"
 	"github.com/docker/docker/pkg/parsers/operatingsystem"
 	"github.com/docker/docker/pkg/platform"
+	"github.com/docker/docker/pkg/rpm"
 	"github.com/docker/docker/pkg/sysinfo"
 	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/registry"
@@ -53,6 +54,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 	// by hand given VERSION)
 	initPath := utils.DockerInitPath("")
 	sysInfo := sysinfo.New(true)
+	packageVersion, _ := rpm.Version("/usr/bin/docker")
 
 	var cRunning, cPaused, cStopped int
 	for _, c := range daemon.List() {
@@ -105,6 +107,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 		HTTPProxy:          getProxyEnv("http_proxy"),
 		HTTPSProxy:         getProxyEnv("https_proxy"),
 		NoProxy:            getProxyEnv("no_proxy"),
+		PkgVersion:         packageVersion,
 	}
 
 	// TODO Windows. Refactor this more once sysinfo is refactored into
@@ -130,6 +133,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 
 // SystemVersion returns version information about the daemon.
 func (daemon *Daemon) SystemVersion() types.Version {
+	pkgVersion, _ := rpm.Version("/usr/bin/docker")
 	v := types.Version{
 		Version:      dockerversion.Version,
 		GitCommit:    dockerversion.GitCommit,
@@ -138,6 +142,7 @@ func (daemon *Daemon) SystemVersion() types.Version {
 		Arch:         runtime.GOARCH,
 		BuildTime:    dockerversion.BuildTime,
 		Experimental: utils.ExperimentalBuild(),
+		PkgVersion:   pkgVersion,
 	}
 
 	if kernelVersion, err := kernel.GetKernelVersion(); err == nil {
