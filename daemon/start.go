c2fa3ee81da2b77011b084631d015d06a87ba15a
Dan Walsh <dwalsh@redhat.com>
2016-01-11 08:29:01 -0500

    Add dockerhooks exec custom hooks for prestart/poststop containers
    With the addition of runc/hooks support we want to add a feature
    to allow third parties to run helper programs before a docker container
    gets started and just after the container finishes.
    
    For example we want to add a RegisterMachine hook.
    
    For systems that support systemd/RegisterMachine, this hook would register
    a machine to the machinectl.  machinectl could then list docker containers
    along with other virtulization environments like kvm, and systemd-nspawn
    containers. Overtime we would want to implement other machinectl features
    to get docker containers better integrated into the system and machinectl.
    
    Another example of a dockerhook might be for people wanting to do better logging
    of starting and stopping of containers.  For example have a log agent that
    records when a container starts and stops and then sends a message to a
    monitoring station.
    
    Dockerhooks reads directory in either /usr/lib/docker/hooks.d or
    /usr/libexec/docker/hooks.d to search for hooks, if the directory exists
    docker will execute the executables in this directory via runc/libcontainer i
    using PreStart and PostStop.  It will also send the config.json file as the
    second paramater.
    
    Signed-off-by: Sally O'Malley <somalley@redhat.com>
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

--- daemon/start.go
+++ daemon/start.go
@@ -136,6 +136,13 @@ func (daemon *Daemon) containerStart(container *container.Container) (err error)
 
 	// don't lock waitForStart because it has potential risk of blocking
 	// which will lead to dead lock, forever.
+
+	jsonPath, err := container.ConfigPath()
+	if err != nil {
+		return err
+	}
+	container.Command.ContainerJSONPath = jsonPath
+
 	if err := daemon.waitForStart(container); err != nil {
 		container.Lock()
 		return err
